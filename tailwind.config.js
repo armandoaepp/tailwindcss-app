e8761d3b9fdb0b797b06cb7dbcfaf0c6ac1dec1a
Armando Enrique Pisfil Puemape <armandoaepp@gmail.com>
2022-08-23 23:21:40 -0500

    dev barra nav 2

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+  ],
+  theme: {
+    container:{
+      padding: '1rem'
+    },
+    extend: {
+      
+      fontFamily: {
+        'sans': "'Work Sans', sans-serif",
+        'cascadia': '"CascadiaCode"',
+      },
+      colors: {
+        'do-blue-dark': '#080c2d',
+        'do-blue-mediumdark': '#1d274c',
+        'do-blue-medium': 'rgb(20, 86, 255)',
+        'do-blue-light': 'rgb(0, 105, 255)',
+        'do-blue-lighter': '#f9fafe',
+      },
+      boxShadow: {
+        'input': '0 5px 1px 0 rgb(0, 0, 0, 0.1)',
+        'input-focus': '0 2px 1px 0 rgb(0, 0, 0, 0.1)',
+      },
+
+    },
+  },
+  plugins: [],
+}
